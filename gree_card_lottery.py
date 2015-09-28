125ce0fb3e933c0d2f48ce16071ec3263bda8b77
Marco Bonvini <bonvini.m@gmail.com>
2015-09-27 21:02:14 -0700

    Added script that generates from the web pages of the Department of State

---
+++ gree_card_lottery.py
@@ -0,0 +1,143 @@
+"""
+Author: Marco Bonvini
+Date: September 27 2015
+
+This script scrapes the pages of the Department of State website to gather statistics and information
+about the Diversity Visa (DV) lottery, aka green card lottery.
+
+The Department of State publishes every month a visa bulletin at
+
+    http://travel.state.gov/content/visas/en/law-and-policy/bulletin.html
+
+this script generates URLs of the bulletins that have been released over the last
+years and look for information. In particular, the script looks for information
+regarding the DV lottery that are typically located in section B of the page,
+
+    B. DIVERSITY IMMIGRANT (DV) CATEGORY FOR THE MONTH OF ...
+
+This section contains a table that states which DV lottery numbers will be able to 
+move forward with their application for the green card.
+The scripts collectes all the data from the tables and generate Pandas DataFrames
+that can be used to anamyze the data and compute some statistics.
+"""
+import urllib2
+import re
+import unicodedata
+from bs4 import BeautifulSoup, Tag
+import pandas as pd
+import numpy as np
+
+# Months and year ranges. Because the bulletins refer to fiscal year (FY), the first month is October.
+years = range(2003,2016)
+months = ['october','november','december','january','february','march','april','may','june','july','august','september']
+
+# Initialize empty pandas DataFrames that will contain the final data for every continent
+df_europe = pd.DataFrame(np.NAN*np.zeros((len(months),len(years))), index = months, columns = years)
+df_africa = pd.DataFrame(np.NAN*np.zeros((len(months),len(years))), index = months, columns = years)
+df_asia = pd.DataFrame(np.NAN*np.zeros((len(months),len(years))), index = months, columns = years)
+df_oceania = pd.DataFrame(np.NAN*np.zeros((len(months),len(years))), index = months, columns = years)
+df_north_america = pd.DataFrame(np.NAN*np.zeros((len(months),len(years))), index = months, columns = years)
+df_south_america = pd.DataFrame(np.NAN*np.zeros((len(months),len(years))), index = months, columns = years)
+
+# When the table contains the string "Current", "current", "c" or "C" instead of the number,
+# replace it with a np.NaN in the DataFrames
+current_number = np.NaN
+
+# The pattern of the URL where one can fond the information about the DV lottery
+pattern = "http://travel.state.gov/content/visas/english/law-and-policy/bulletin/{0}/visa-bulletin{3}-{1}-{2}.html"
+
+# Iterate over all the years and months, generate the URLs and look for the
+# data regarding the DV lottery. For each year of each month multiple URLS are built.
+# This is necessary because the URLs haven't been managed in a consistent way over the years,
+# for example sometimes the month starts with a lowercase letter but in other cases it does not.
+for fy in years:
+	for m in months:
+                # Initialize list of URLs to check
+		page_urls = []
+
+                # The months of Oct, Nov and Dec needs to be treated differently
+                # because, for example, Oct 2014 belongs to FY 2015.
+		if m in ['october','november','december']:
+			page_urls.append(pattern.format(fy,m,fy-1,"-for"))
+			page_urls.append(pattern.format(fy,m,fy-1,""))
+			page_urls.append(pattern.format(fy,m.title(),fy-1,""))
+			page_urls.append(pattern.format(fy,m.title(),fy-1,"-for"))
+		else:
+			page_urls.append(pattern.format(fy,m,fy,"-for"))
+			page_urls.append(pattern.format(fy,m,fy,""))
+			page_urls.append(pattern.format(fy,m.title(),fy,""))
+			page_urls.append(pattern.format(fy,m.title(),fy,"-for"))
+
+                # Test the URLs, verify if they're legitimate, and get the
+                # content of the HTML page
+		page_ok = False
+		i = 0
+		while not page_ok and i<len(page_urls):
+			print "Reading page: {0}".format(page_urls[i])
+                        try:
+                                response = urllib2.urlopen(page_urls[i])
+			        html_text = response.read()
+		        
+			        if "404 - Page Not Found" not in html_text:
+				        page_ok = True
+                        except urllib2.HTTPError, e:
+                                print "Failed reading the URL: {0}".format(page_urls[i])
+                                print str(e)
+                                
+			i += 1
+
+                # If there was a successful read, parse the HTML page and populate the
+                # columns/rows of the pandas DataFrames
+		if page_ok:
+			soup = BeautifulSoup(html_text)
+			soup.encode('utf-8')
+			
+			for tag in soup.find_all('script'):
+				tag.replaceWith('')
+			
+			text = unicode(soup.get_text('|', strip=True))
+			text = text.encode('ascii',errors='ignore')
+			text = re.sub(',','',text)
+			text = re.sub('\\r','',text)
+			text = re.sub('\\n','',text)
+			text = re.sub(':','',text)
+			text = re.sub('-','',text)
+			text = re.sub('\s+','',text)
+			text = text.lower()
+                        
+			g_europe = re.search('(europe[|]*[\s*eu[r]*\s*]*[|]*(\d+|current|c))', text)
+			if g_europe:
+				df_europe[fy][m] = g_europe.groups()[1] if g_europe.groups()[1] not in ['c','current'] else current_number
+			else:
+				print "Missing Europe..."
+				
+			g_africa = re.search('(africa[|]*[\s]*[af]*[\s]*[|]*(\d+|current|c))', text)
+			if g_africa:
+				df_africa[fy][m] = g_africa.groups()[1] if g_africa.groups()[1] not in ['c','current'] else current_number
+			else:
+				print "Missing Africa..."
+		
+			g_oceania = re.search('(oceania[|]*[\s]*[oc]*[\s]*[|]*(\d+|current|c))', text)
+			if g_oceania:
+				df_oceania[fy][m] = g_oceania.groups()[1] if g_oceania.groups()[1] not in ['c','current'] else current_number
+			else:
+				print "Missing Oceania..."
+			
+			g_asia = re.search('(asia[|]*[\s]*[as]*[\s]*[|]*(\d+|current|c))', text)
+			if g_asia:
+				df_asia[fy][m] = g_asia.groups()[1] if g_asia.groups()[1] not in ['c','current'] else current_number
+			else:
+				print "Missing Asia..."
+				
+			g_north_america = re.search('(northamerica[|]*\(bahamas\)[|]*[\s]*[na]*[\s]*[|]*(\d+|current|c))', text)
+			if g_north_america:
+				df_north_america[fy][m] = g_north_america.groups()[1] if g_north_america.groups()[1] not in ['c','current'] else current_number
+			else:
+				print "Missing North America..."
+			
+			g_south_america = re.search('(southamerica[|]*and[|]*the[|]*caribbean[|]*[\s]*[sa]*[\s]*[|]*(\d+|current|c))', text)
+			if g_south_america:
+				df_south_america[fy][m] = g_south_america.groups()[1] if g_south_america.groups()[1] not in ['c','current'] else current_number
+			else:
+				print "Missing South America..."
+				
